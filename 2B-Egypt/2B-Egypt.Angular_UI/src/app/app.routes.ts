17ec351cfbe9ada7385837a5342bbeecb42dd417
Nada Ossama <118224738+NadaOssama2001@users.noreply.github.com>
2024-10-19 13:33:34 +0300

    Angular

--- 2B-Egypt/2B-Egypt.Angular_UI/src/app/app.routes.ts
+++ 2B-Egypt/2B-Egypt.Angular_UI/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './Shared/Components/product-list/product-list.component';
 
 export const routes: Routes = [
-    { path: 'products', component: ProductListComponent }, 
-    { path: '', redirectTo: '/products', pathMatch: 'full' }
+    { path: '', component: ProductListComponent },
+    { path: 'products', component: ProductListComponent }
+    
 ];
--- 2B-Egypt/2B-Egypt.Angular_UI/src/main.ts
+++ 2B-Egypt/2B-Egypt.Angular_UI/src/main.ts
@@ -1,8 +1,30 @@
-/// <reference types="@angular/localize" />
+// import { provideHttpClient } from '@angular/common/http';
+// import { provideRouter } from '@angular/router';
+// import { routes } from './app/app.routes';
+// import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// // import { appConfig } from './app/app.config';
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+// // bootstrapApplication(AppComponent, appConfig)
+// //   .catch((err) => console.error(err));
+
+// export const appConfig = {
+//   providers: [
+//     provideHttpClient(),  // Provide HttpClientModule here
+//     provideRouter(routes), // Provide router if using routing
+//     // Additional services or configurations can go here
+//   ],
+// };
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+  ]
+});
+
