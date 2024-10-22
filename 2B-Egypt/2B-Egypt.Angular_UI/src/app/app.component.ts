47ff670928e5705d80a87e961068f219cd9998c5
WALID Walid Nader <Walid.Nader@it-fusion.org>
2024-10-22 14:31:28 +0200

    Product List Handler

--- 2B-Egypt/2B-Egypt.Angular_UI/src/app/app.component.ts
+++ 2B-Egypt/2B-Egypt.Angular_UI/src/app/app.component.ts
@@ -1,30 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './Shared/Components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LanguageServiceService } from './services/language-service.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductListComponent, RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet,ProductListComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
   title = '2B-Egypt.Angular_UI';
-  lang: string = '';
-  constructor(private _LanguageService: LanguageServiceService) {}
-  ngOnInit(): void {
-    this._LanguageService.getlanguage().subscribe({
-      next: (lang) => {
-        this.lang = lang;
-      },
-    });
-  }
-  changelang() {
-    this._LanguageService.cahngelanguage(this.lang == 'en' ? 'ar' : 'en');
-  }
-
 }
