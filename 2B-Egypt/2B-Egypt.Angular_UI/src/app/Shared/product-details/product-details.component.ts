7bc807978ab6f13f8ed61219c79f9d8b94656037
WALID Walid Nader <Walid.Nader@it-fusion.org>
2024-10-23 21:52:42 +0200

    Reviews Design

--- 2B-Egypt/2B-Egypt.Angular_UI/src/app/Shared/product-details/product-details.component.ts
+++ 2B-Egypt/2B-Egypt.Angular_UI/src/app/Shared/product-details/product-details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { IProduct } from '../../../models/IProduct';
 import { FormsModule } from '@angular/forms'; 
 import { CommonModule, JsonPipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import Cookies from 'js-cookie';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { IReview } from '../../../models/ireview';
+import { ReviewServiceService } from '../../services/review-service.service';
 
 @Component({
   selector: 'app-product-details',
@@ -19,14 +21,35 @@ export class ProductDetailsComponent implements OnInit {
   productId: string | null = null;
   PriceAfterSale: number = 0;
 IsMoreInfo:boolean=true;
+rating: number = 0;  
+Review:IReview= {} as IReview;
+isLoading: boolean =true;
+// stars part
+@Input() ratingPrice: number = 0;
+  @Input() ratingQuilty: number = 0; 
+  @Input() ratingValue: number = 0; 
+  @Input() starCount: number = 5; 
+  @Output() ratingUpdated = new EventEmitter<number>();
+  starsPrice: boolean[] = [false,false,false,false,false];
+  starsQuilty: boolean[] = [false,false,false,false,false];
+  starsValue: boolean[] = [false,false,false,false,false];
+  // stars part
+
   constructor(
     private _productService: ProductService,
     private route: ActivatedRoute,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private _ReviewService : ReviewServiceService,
+    private router : Router
   ) {}
-
+  
   ngOnInit() {
     this.productId = this.route.snapshot.params['id'];
+    this.Review.productId = this.route.snapshot.params['id'];
+this.Review.priceRating = this.ratingPrice.toString();
+this.Review.qualityRating = this.ratingQuilty.toString();
+this.Review.valueRating = this.ratingValue.toString();
+    // console.log(this.productId); // طباعة القيم للتحقق منها
     if (this.productId) {
       this._productService.getProductById(this.productId).subscribe({
         next: (res) => {
@@ -64,5 +87,51 @@ IsMoreInfo:boolean=true;
   }
   activateTab(showMoreInfo: boolean) {
     this.IsMoreInfo = showMoreInfo; 
+  }
+//stars parts
+  ratePrice(rating: number) {
+    this.ratingPrice = rating;
+    this.Review.priceRating =rating.toString();
+    this.ratingUpdated.emit(this.ratingPrice); // Emit the new rating
+    console.log('ratingPrice' , this.ratingPrice)
+  }
+
+  rateQuilty(rating: number) {
+    this.ratingQuilty = rating;
+    this.Review.qualityRating =rating.toString();
+    this.ratingUpdated.emit(this.ratingPrice); // Emit the new rating
+    console.log('ratingQuilty' , this.ratingQuilty)
+  }
+
+  rateValue(rating: number) {
+    this.ratingValue = rating;
+    this.Review.valueRating =rating.toString();
+    this.ratingUpdated.emit(this.ratingValue); // Emit the new rating
+    console.log('ratingValue' , this.ratingValue)
+  }
+//stars parts
+
+
+
+
+
+
+
+
+
+
+  addreview() {
+    // console.log(this.Review); // طباعة القيم للتحقق منها
+    this._ReviewService.addReview(this.Review).subscribe({
+      
+      next: (res) => {
+        alert("Review Added Successfully")
+        // this.router.navigateByUrl(`product-details/${this.productId}`);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    
+    });
   }
 }
